605dbc2a22e3bb75b68c8ef90b244e5ac992e447
James Parkhurst <jmp@observatorysciences.co.uk>
2026-05-21 10:46:50 +0100

    Adding run command

--- src/dragonET/command_line/_run.py
+++ src/dragonET/command_line/_run.py
@@ -6,12 +6,19 @@
 # Author: James Parkhurst
 #
 import time
+import os
 from argparse import ArgumentParser
 from typing import List
 
-import mrcfile # type: ignore
-import numpy as np
-import yaml
+
+from dragonET.command_line._new import _new
+from dragonET.command_line._track import _track
+from dragonET.command_line._generate_angles import _generate_angles
+from dragonET.command_line._reconstruct import _reconstruct
+from dragonET.command_line._refine import _refine
+from dragonET.command_line._stack_rebin import _stack_rebin
+from dragonET.command_line._stack_transform import _stack_transform
+
 
 __all__ = ["run"]
 
@@ -120,13 +127,13 @@ def run(args: List[str] | None = None):
     Run the automated pipeline
 
     """
-    new_impl(get_parser().parse_args(args=args))
+    run_impl(get_parser().parse_args(args=args))
 
 
 def _run(
     projections_filename: str,
     angles_filename: str,
-    global_rotation: float =0,
+    global_rotation: float = 0,
     rebin_factor: int = 1,
     device: str = "gpu",
 ):
@@ -141,59 +148,106 @@ def _run(
     refined_model_fix_bc_filename = os.path.join("output", "refined_model_fix_bc.yaml")
     refined_model_fix_c_filename = os.path.join("output", "refined_model_fix_c.yaml")
     rebinned_projections_filename = os.path.join("output", "rebinned_projections.mrc")
+    aligned_projections_tracked_filename = os.path.join(
+        "output", "aligned_projections_tracked.mrc"
+    )
+    aligned_projections_fix_bc_filename = os.path.join(
+        "output", "aligned_projections_fix_bc.mrc"
+    )
+    aligned_projections_fix_c_filename = os.path.join(
+        "output", "aligned_projections_fix_c.mrc"
+    )
     volume_fix_bc_filename = os.path.join("output", "volume_fix_bc.mrc")
     volume_fix_c_filename = os.path.join("output", "volume_fix_c.mrc")
-    
-    
+
+    # If no angles are specified
+    if angles_filename is None or not os.path.exists(angles_filename):
+        # Set the angles filename
+        angles_filename = os.path.join("output", "angles.rawtlt")
+
+        # And generate some new angles
+        _generate_angles(projections_filename, angles_filename)
+
     # Import the experimental data and create initial model
-    _new(
-        projections_filename, 
-        angles_filename, 
-        initial_model_filename, 
-        global_rotation
-    )
+    if not os.path.exists(initial_model_filename):
+        _new(
+            projections_filename,
+            angles_filename,
+            initial_model_filename,
+            global_rotation,
+        )
 
     # Find features and track them across images
-    _track(
-        projections_filename, 
-        initial_model_filename, 
-        tracked_model_filename, 
-        tracked_contours_filename
-    )
+    if not os.path.exists(tracked_contours_filename):
+        _track(
+            projections_filename,
+            initial_model_filename,
+            tracked_model_filename,
+            tracked_contours_filename,
+        )
 
     # Refine the initial model based on the contours (fix=bc)
-    _refine(
-        initial_model_filename,
-        refined_model_fix_bc_filename,
-        tracked_contours_filename,
-        fix="bc",
-    )
-    
+    if not os.path.exists(refined_model_fix_bc_filename):
+        _refine(
+            initial_model_filename,
+            refined_model_fix_bc_filename,
+            tracked_contours_filename,
+            fix="bc",
+        )
+
     # Rerefine the model based on the contours (fix=c)
-    _refine(
-        refined_model_fix_bc_filename,
-        refined_model_fix_c_filename,
-        tracked_contours_filename,
-        fix="c",
-    )
+    if not os.path.exists(refined_model_fix_c_filename):
+        _refine(
+            refined_model_fix_bc_filename,
+            refined_model_fix_c_filename,
+            tracked_contours_filename,
+            fix="c",
+        )
 
     # Rebin the data
-    _stack_rebin(projections_filename, rebinned_projections_filename, rebin_factor)
+    if not os.path.exists(rebinned_projections_filename):
+        _stack_rebin(projections_filename, rebinned_projections_filename, rebin_factor)
+
+    # Make an aligned stack from the tracked projections
+    if not os.path.exists(aligned_projections_tracked_filename):
+        _stack_transform(
+            rebinned_projections_filename,
+            aligned_projections_tracked_filename,
+            tracked_model_filename,
+        )
+
+    # Make an aligned stack from the fix bc projections
+    if not os.path.exists(aligned_projections_fix_bc_filename):
+        _stack_transform(
+            rebinned_projections_filename,
+            aligned_projections_fix_bc_filename,
+            refined_model_fix_bc_filename,
+        )
+
+    # Make an aligned stack from the fix c projections
+    if not os.path.exists(aligned_projections_fix_c_filename):
+        _stack_transform(
+            rebinned_projections_filename,
+            aligned_projections_fix_c_filename,
+            refined_model_fix_c_filename,
+        )
 
     # Reconstruct the volume from the fix bc model
-    _reconstruct(
-        rebinned_projections_filename,
-        refined_model_fix_bc_filename,
-        volume_fix_bc_filename,
-        num_iterations=10
-        device=device,
-    )
+    if not os.path.exists(volume_fix_bc_filename):
+        _reconstruct(
+            rebinned_projections_filename,
+            refined_model_fix_bc_filename,
+            volume_fix_bc_filename,
+            num_iterations=10,
+            device=device,
+        )
 
     # Reconstruct the volume from the fix c model
-    _reconstruct(
-        rebinned_projections_filename,
-        refined_model_fix_c_filename,
-        volume_fix_c_filename,
-        num_iterations=10
-        device=device,
-    )
+    if not os.path.exists(volume_fix_c_filename):
+        _reconstruct(
+            rebinned_projections_filename,
+            refined_model_fix_c_filename,
+            volume_fix_c_filename,
+            num_iterations=10,
+            device=device,
+        )
